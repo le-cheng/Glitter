6c9b6d39b9f541405f6094f42307308c3ff6f191
le-cheng <chengleniubi@163.com>
2024-06-28 10:59:55 +0800

    ninja
    Signed-off-by: le-cheng <chengleniubi@163.com>

--- clean.sh
+++ clean.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# # 检查build目录是否存在
+# if [ -d "build" ]; then
+#     echo "Directory 'build' exists. Clearing its contents..."
+#     rm -rf build/*
+# else
+#     echo "Directory 'build' does not exist. Creating it..."
+#     mkdir -p build
+# fi
+
 # 检查build目录是否存在
-if [ -d "build" ]; then
+if [ -d "CMakeFiles" ]; then
     echo "Directory 'build' exists. Clearing its contents..."
-    rm -rf build/*
-else
-    echo "Directory 'build' does not exist. Creating it..."
-    mkdir -p build
+    rm -rf CMakeFiles/
 fi
+
+ninja -t clean
 
 echo "clean completed."
--- run_build.sh
+++ run_build.sh
@@ -7,18 +7,23 @@ else
     echo "Directory 'build' does not exist. Creating it..."
     mkdir -p build
 fi
-
+# 进入build目录
 cd build
 
-# 设置CMake参数，例如启用优化或调试模式
-# CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Release"
 
-# # 使用CMake配置项目
-cmake -G "MinGW Makefiles" ..
+# # 设置CMake参数，例如启用优化或调试模式
+# # CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Release"
 
-# 使用MinGW编译项目
-mingw32-make -j8
+# # # 使用CMake配置项目
+# cmake -G "MinGW Makefiles" ..
 
+
+# # 使用MinGW编译项目
+# mingw32-make -j16
+
+cmake -G Ninja ..
+ninja -j6
+ 
 # ./out/main.exe
 
 echo "run completed."
